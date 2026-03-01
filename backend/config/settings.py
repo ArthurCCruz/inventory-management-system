1fe551215673a757ac5d69336415d0826bf49e9f
ArthurCCruz <ccruz.arthur@gmail.com>
2026-03-01 17:14:30 -0300

    [FIX] add csrf trusted origins

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -162,6 +162,11 @@ CSRF_COOKIE_SAMESITE = "None"
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+])
+
 # REST Framework settings
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
